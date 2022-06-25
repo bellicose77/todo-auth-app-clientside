c9d8974387481d8aee547e7a51d5670f1e76001b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-25 23:51:34 +0600

    handle user data

--- todo-crud-auth/src/components/AddTask/AddTask.js
+++ todo-crud-auth/src/components/AddTask/AddTask.js
@@ -23,15 +23,15 @@ const AddTask = () => {
     <Form onSubmit={handelSubmit}>
     <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Title</Form.Label>
-    <Form.Control name="title" className="mb-3" type="text"  />
+    <Form.Control onChange={handleUserInput} name="title" className="mb-3" type="text"  />
     </Form.Group>
     <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Description</Form.Label>
-    <Form.Control name="Description" type="text" />
+    <Form.Control onChange={handleUserInput}  name="Description" type="text" />
     </Form.Group>
     <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Created Date</Form.Label>
-    <Form.Control name="date" type="date" />
+    <Form.Control onChange={handleUserInput}  name="date" type="date" />
     </Form.Group>
    <Button variant="primary" type="submit">
     Submit
