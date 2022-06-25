3c11c8ba2350f5e3a7441e74bca2ba7d822df73b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-25 21:53:32 +0600

    react-router

--- todo-crud-auth/src/App.js
+++ todo-crud-auth/src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import './App.css';
+import AddTask from './components/AddTask/AddTask';
 import Home from './components/Home/Home';
 import Nabigation from './components/Nabigation/Nabigation';
 
@@ -11,6 +12,7 @@ function App() {
       <Nabigation/>
       <Routes>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/tasks" element={<AddTask/>}/>
       </Routes>
       </Router>
      
