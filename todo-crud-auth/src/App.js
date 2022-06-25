44bbb42cfc5f6d9415c8889cfe080565bb81571d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-25 21:54:49 +0600

    react-router

--- todo-crud-auth/src/App.js
+++ todo-crud-auth/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import AddTask from './components/AddTask/AddTask';
 import Home from './components/Home/Home';
 import Nabigation from './components/Nabigation/Nabigation';
+import ShowTask from './components/ShowTask/ShowTask';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home/>}/>
         <Route path="/tasks" element={<AddTask/>}/>
+        <Route path="/show" element={<ShowTask/>}/>
       </Routes>
       </Router>
      
