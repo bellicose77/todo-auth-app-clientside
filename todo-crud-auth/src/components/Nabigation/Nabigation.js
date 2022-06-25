af795eda2b6a1d94efd14bd6c48d52cf4e74dadd
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-25 21:20:04 +0600

    navbar

--- todo-crud-auth/src/components/Nabigation/Nabigation.js
+++ todo-crud-auth/src/components/Nabigation/Nabigation.js
@@ -15,17 +15,7 @@ const Nabigation = () => {
           <Nav className="me-auto">
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">ADD TASK</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              {/* <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item> */}
-            </NavDropdown>
+           
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
