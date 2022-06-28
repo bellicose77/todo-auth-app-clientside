a33fe71b558b1db29b10f095730a83216c4bb585
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-29 00:11:38 +0600

    update api

--- todo-crud-auth/src/components/UpdateTodo/UpdateTodo.js
+++ todo-crud-auth/src/components/UpdateTodo/UpdateTodo.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateTodo = () => {
+    const {id} = useParams();
     return (
         <div>
             <h2>Update your todo</h2>
