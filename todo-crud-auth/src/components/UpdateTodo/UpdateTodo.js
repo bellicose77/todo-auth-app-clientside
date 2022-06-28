6eb58654379b5ac514a809a4a54985b50868175b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-29 00:22:54 +0600

    update form

--- todo-crud-auth/src/components/UpdateTodo/UpdateTodo.js
+++ todo-crud-auth/src/components/UpdateTodo/UpdateTodo.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const UpdateTodo = () => {
     const {id} = useParams();
     return (
         <div>
-            <h2>Update your todo</h2>
+            <h2>Update your todo:{id}</h2>
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Title</Form.Label>
+    <Form.Control name="title" className="mb-3" type="text"  />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Description</Form.Label>
+    <Form.Control name="description" type="text" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Created Date</Form.Label>
+    <Form.Control name="date" type="date" />
+    </Form.Group>
+   <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
         </div>
     );
 };
