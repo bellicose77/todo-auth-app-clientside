ce1b65ed7ad5cc58d15e77933e2d574d81c216a0
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-29 20:18:41 +0600

    update value set in state

--- todo-crud-auth/src/components/UpdateTodo/UpdateTodo.js
+++ todo-crud-auth/src/components/UpdateTodo/UpdateTodo.js
@@ -16,15 +16,15 @@ const UpdateTodo = () => {
     <Form>
     <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Title</Form.Label>
-    <Form.Control name="title" value={singleTodo.title} onChange={handleUpadateName} className="mb-3" type="text"  />
+    <Form.Control name="title" value={singleTodo.title} onChange={handleUpadateTitle} className="mb-3" type="text"  />
     </Form.Group>
     <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Description</Form.Label>
-    <Form.Control name="description" value={singleTodo.description} onChange={handleUpadateName}  type="text" />
+    <Form.Control name="description" value={singleTodo.description} onChange={handleUpadateDesc}  type="text" />
     </Form.Group>
     <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Created Date</Form.Label>
-    <Form.Control name="date" value={singleTodo.date} type="date" />
+    <Form.Control name="date" value={singleTodo.date} onChange={handleUpadateDate}  type="date" />
     </Form.Group>
    <Button variant="primary" type="submit">
     Submit
