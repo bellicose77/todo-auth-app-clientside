19b0919aafb8d811a683d454cdeb9de849c177d8
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-05 20:24:51 +0600

    login part starting

--- todo-crud-auth/src/components/UserLogin/UserLogin.js
+++ todo-crud-auth/src/components/UserLogin/UserLogin.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const UserLogin = () => {
     return (
         <div>
             <h2>Login Here</h2>
+
+        <Button variant='success'>Sign IN with google</Button>
         </div>
     );
 };
