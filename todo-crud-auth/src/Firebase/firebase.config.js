36d3ddd295be9026f80dc58af4aa2a7d668b0101
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-05 23:59:09 +0600

    firebase auth

---
+++ todo-crud-auth/src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqFR91QV0OLep6ZI_z4MlTQAWCNs1TNA0",
+    authDomain: "react-todo-authenticatio-e461b.firebaseapp.com",
+    projectId: "react-todo-authenticatio-e461b",
+    storageBucket: "react-todo-authenticatio-e461b.appspot.com",
+    messagingSenderId: "775719703700",
+    appId: "1:775719703700:web:875324ac7e2276086dd081"
+  };
+
+  export default firebaseConfig;
