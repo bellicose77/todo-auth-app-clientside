f3a498c84b3aa87585da5b8830dd03a0bb13350b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-13 00:09:44 +0600

    google sign in

--- todo-crud-auth/src/components/UserLogin/UserLogin.js
+++ todo-crud-auth/src/components/UserLogin/UserLogin.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import useFirebase from '../../hooks/useFirebase';
 
 
 const UserLogin = () => {
+    const {signInByGoogle} = useFirebase()
     return (
         <div >
             <h2>Login Here</h2>
 
-        <Button onClick={} variant='success'>Sign IN with google</Button>
+        <Button onClick={signInByGoogle} variant='success'>Sign IN with google</Button>
         </div>
     );
 };
