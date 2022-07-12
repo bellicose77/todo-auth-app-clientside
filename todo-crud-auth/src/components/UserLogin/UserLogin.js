98cc861fd8fbd7351b9e9a93566c3a51055c372d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-13 00:08:11 +0600

    google sign in

--- todo-crud-auth/src/components/UserLogin/UserLogin.js
+++ todo-crud-auth/src/components/UserLogin/UserLogin.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const handleSignIn = () =>{
-    console.log("handle sign In");
-}
 
 const UserLogin = () => {
     return (
         <div >
             <h2>Login Here</h2>
 
-        <Button onClick={handleSignIn} variant='success'>Sign IN with google</Button>
+        <Button onClick={} variant='success'>Sign IN with google</Button>
         </div>
     );
 };
