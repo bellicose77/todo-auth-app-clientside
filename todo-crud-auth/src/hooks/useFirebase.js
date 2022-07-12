45f91560c6362ea77af9feff2d4d63dbe8503f5a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-12 23:57:34 +0600

    google sign in

--- todo-crud-auth/src/hooks/useFirebase.js
+++ todo-crud-auth/src/hooks/useFirebase.js
@@ -1,8 +1,9 @@
 import initilizationauthentication from "../Firebase/firebase.initilize";
+import { GoogleAuthProvider } from "firebase/auth";
 
 initilizationauthentication();
 const useFirebase = () =>{
-
+         const googleProvider = new GoogleAuthProvider()
 }
 
 export default useFirebase;
