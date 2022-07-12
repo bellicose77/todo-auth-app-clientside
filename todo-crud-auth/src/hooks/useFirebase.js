91fd6a8ff01554d3ac8971735fe19d4016952e5a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-13 00:05:33 +0600

    google sign in

--- todo-crud-auth/src/hooks/useFirebase.js
+++ todo-crud-auth/src/hooks/useFirebase.js
@@ -1,12 +1,14 @@
 import initilizationauthentication from "../Firebase/firebase.initilize";
 import { GoogleAuthProvider,getAuth, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 initilizationauthentication();
 const useFirebase = () =>{
+    const [user ,setUser] = useState({});
       const googleProvider = new GoogleAuthProvider();
       const auth = getAuth();
 
-      const signInGoogle = () =>{
+      const signInByGoogle = () =>{
            signInWithPopup(auth,googleProvider)
       }
 }
