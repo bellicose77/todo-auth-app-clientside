5380c53c5e860c26b912fb414dd187d8f11dd541
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-13 23:37:21 +0600

    auth context

--- todo-crud-auth/src/components/UserLogin/UserLogin.js
+++ todo-crud-auth/src/components/UserLogin/UserLogin.js
@@ -7,7 +7,7 @@ const UserLogin = () => {
     const {signInByGoogle,user} = useFirebase()
     return (
         <div >
-            <h2>Login Here</h2>
+            <h2>Login Here:{user.displayName}</h2>
 
         <Button onClick={signInByGoogle} variant='success'>Sign IN with google</Button>
         </div>
