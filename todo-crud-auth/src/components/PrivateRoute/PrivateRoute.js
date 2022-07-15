6e04ad1eebe873669f8747bbef29de9bd45f24ea
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-15 11:44:53 +0600

    private route

--- todo-crud-auth/src/components/PrivateRoute/PrivateRoute.js
+++ todo-crud-auth/src/components/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {useLocation} from 'react-router-dom'
+import {Navigate, useLocation} from 'react-router-dom'
 import useAuth from '../../hooks/useAuth';
 const PrivateRoute = ({children,...rest}) => {
    const{user} = useAuth();
    const location = useLocation();
   if(user.email){
     return children;
+  }
+  else{
+    return <Navigate to="/login" state={{ from: location }} />
   }
 };
 
