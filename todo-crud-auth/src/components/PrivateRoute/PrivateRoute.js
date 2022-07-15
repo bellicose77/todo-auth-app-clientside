437a87b103233b34b129ebb04426fbe48d49a052
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-15 11:34:48 +0600

    private route

--- todo-crud-auth/src/components/PrivateRoute/PrivateRoute.js
+++ todo-crud-auth/src/components/PrivateRoute/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import useAuth from '../../hooks/useAuth';
 const PrivateRoute = ({children,...rest}) => {
-
+   const{user} = useAuth();
 };
 
 export default PrivateRoute;
